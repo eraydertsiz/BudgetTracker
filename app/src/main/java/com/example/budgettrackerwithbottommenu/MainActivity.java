709ff50ae427b41e41eacbf7f063284cd4f37a61
ahmetrukenboyaci <ahmetrukenboyaci@gmail.com>
2020-01-05 17:54:33 +0300

    PieChartLibrary added

--- app/src/main/java/com/example/budgettrackerwithbottommenu/MainActivity.java
+++ app/src/main/java/com/example/budgettrackerwithbottommenu/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import lecho.lib.hellocharts.view.PieChartView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
