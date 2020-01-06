8123d8791b69de7ef33ed2cd344bfa7380ec1a25
eraydertsiz <edertsiz1@gmail.com>
2020-01-06 03:45:09 +0300

    activity_layout fragment_home'a taşındı

--- app/src/main/java/com/example/budgettrackerwithbottommenu/MainActivity.java
+++ app/src/main/java/com/example/budgettrackerwithbottommenu/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,13 +22,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        //NavigationUI.setupWithNavController(navView, navController);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        /*
         PieChartView pieChartView = findViewById(R.id.chart);
         Button tradeBtn = (Button) findViewById(R.id.btnAlisveris);
         Button socialBtn = (Button) findViewById(R.id.btnSosyal);
@@ -41,38 +46,45 @@ public class MainActivity extends AppCompatActivity {
         tradeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                bottomSheet.putExtra("category", "trade");
                 startActivity(bottomSheet);
             }
         });
         socialBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                bottomSheet.putExtra("category", "social");
                 startActivity(bottomSheet);
             }
         });
         transportBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                bottomSheet.putExtra("category", "transport");
                 startActivity(bottomSheet);
             }
         });
         healthBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                bottomSheet.putExtra("category", "health");
                 startActivity(bottomSheet);
             }
         });
         giftBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                bottomSheet.putExtra("category", "gift");
                 startActivity(bottomSheet);
             }
         });
         billBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                bottomSheet.putExtra("category", "bill");
                 startActivity(bottomSheet);
             }
         });
+        */
     }
 }
